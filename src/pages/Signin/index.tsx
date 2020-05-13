760fc1bff30c41f0eeb04787950f1050692631a9
Lucas Duarte <l.duarte.mk@gmail.com>
2020-05-13 16:18:09 -0300

    global style

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container, Content, Background } from './styles';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
@@ -8,11 +11,20 @@ const SignIn: React.FC = () => (
   <Container>
     <Content>
       <img src={logoImg} alt="QAgil" />
+
       <form>
         <h1>Faça Seu Logon</h1>
-        <input type="text" placeholder="E-Mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+
+        <Input name="email" icon={FiMail} type="text" placeholder="E-Mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Entrar</Button>
+
         <a href="forgot">Esqueci minha senha</a>
       </form>
       <a href="account">
