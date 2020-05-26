06732067a06fc92785d838bea3c755e1464e3e6b
Lucas Duarte <l.duarte.mk@gmail.com>
2020-05-26 19:11:16 -0300

    error validations

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Container, Content, Background } from './styles';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import getValidarionErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -10,8 +12,13 @@ import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
 
 const SingUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  console.log(formRef);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -25,6 +32,9 @@ const SingUp: React.FC = () => {
       });
     } catch (err) {
       console.log(err);
+
+      const errors = getValidarionErrors(err);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -34,7 +44,7 @@ const SingUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="QAgil" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça Seu Cadastro</h1>
 
           <Input name="name" icon={FiUser} type="text" placeholder="E-Mail" />
