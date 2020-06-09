37bc7cabd76b91a4aee738809d8b9d0e76e92c02
Lucas Duarte <l.duarte.mk@gmail.com>
2020-06-09 11:23:33 -0300

    troca de telas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import CreateEvent from '../EndPoints/createEvent';
+import UpdateEvent from '../EndPoints/updateEvent';
 
-const Dashboard: React.FC = () => (
-  <>
-    <CreateEvent />
-  </>
-);
+import { List } from './styles';
+
+const Dashboard: React.FC = () => {
+  const [formAtual, setFormAtual] = React.useState('createEvent');
+  const telas: { [key: string]: any } = {
+    createEvent: <CreateEvent />,
+    updateEvent: <UpdateEvent />,
+  };
+
+  return (
+    <>
+      {telas[formAtual]}
+
+      <List>
+        <h1>Event Services</h1>
+        <span onClick={() => setFormAtual('createEvent')}>
+          Create Event
+        </span>{' '}
+        <br />
+        <span onClick={() => setFormAtual('updateEvent')}>
+          Update Event
+        </span>{' '}
+        <br />
+      </List>
+    </>
+  );
+};
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const List = styled.div`
+  color: #fff;
+  position: absolute;
+
+  right: 0;
+  top: 0;
+  margin-right: 50px;
+  margin-top: 50px;
+
+  span {
+    &:hover {
+      color: #ff9000;
+      cursor: pointer;
+    }
+  }
+`;
