85a024c32381954e4790171a55f6908f2f62e62e
Lucas Duarte <l.duarte.mk@gmail.com>
2020-06-13 12:30:46 -0300

    Updates

---
+++ precache-manifest.f72a4b1f67bfff5dde29a08020bfc857.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a45e6d5c6ebe4e09ae33259a5b6cd3e",
+    "url": "/qagil-web/index.html"
+  },
+  {
+    "revision": "db5fe5d3b5482bafd79c",
+    "url": "/qagil-web/static/js/2.dbee85b4.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/qagil-web/static/js/2.dbee85b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b907f3a9955f7fa88c",
+    "url": "/qagil-web/static/js/main.7cb0e84f.chunk.js"
+  },
+  {
+    "revision": "7a9344e0429cf44d7cdc",
+    "url": "/qagil-web/static/js/runtime-main.8b80dff9.js"
+  },
+  {
+    "revision": "c845baefe4e502b83cd22505fb0b61e8",
+    "url": "/qagil-web/static/media/logo.c845baef.png"
+  },
+  {
+    "revision": "a86e46dd4877aa81e02ac61eb5013887",
+    "url": "/qagil-web/static/media/sign-in-background.a86e46dd.jpg"
+  },
+  {
+    "revision": "990d9793a1950f627aaf5e90dc007d77",
+    "url": "/qagil-web/static/media/sing-up-background.990d9793.jpeg"
+  }
+]);
