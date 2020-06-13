ae55f54e0bd3720f34a5b4566f92e5ffb8116bdf
Lucas Duarte <l.duarte.mk@gmail.com>
2020-06-13 15:12:21 -0300

    header added name and logout function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import logoImg from '../../assets/logo.png';
+import { AiOutlinePoweroff } from 'react-icons/all';
+import { useUserBackend } from '../../hooks/userBackend';
+import { useAuth } from '../../hooks/auth';
 
-import { Container } from './styles';
+import {
+  Container,
+  Logo,
+  ProfileInfos,
+  SubContainer,
+  UserName,
+} from './styles';
 
 const Header: React.FC = () => {
+  const { userBackEnd }: any = useUserBackend();
+  const { singOut, user }: any = useAuth();
+  console.log(user);
+
   return (
     <Container>
-      <img src={logoImg} alt="QAgil" />
+      <SubContainer>
+        <Logo src={logoImg} alt="QAgil" />
+
+        <ProfileInfos>
+          <img
+            src={
+              'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
+            }
+            alt="profile Picture"
+          />
+          <div>
+            <span>Bem Vindo</span>
+            {userBackEnd && <UserName>{user.name}</UserName>}
+          </div>
+        </ProfileInfos>
+      </SubContainer>
+      <AiOutlinePoweroff onClick={singOut} />
     </Container>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,8 +6,46 @@ export const Container = styled.div`
   height: 100px;
   display: flex;
   align-items: center;
+  display: flex;
+  justify-content: space-around;
 
   img {
     width: 220px;
   }
+
+  svg {
+    cursor: pointer;
+    color: #999591;
+  }
+`;
+
+export const Logo = styled.img`
+  width: 220px;
+`;
+
+export const ProfileInfos = styled.div`
+  display: flex;
+  width: 200px;
+  justify-content: space-around;
+  align-items: center;
+
+  div {
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+  }
+
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+  }
+`;
+
+export const SubContainer = styled.div`
+  display: flex;
+`;
+
+export const UserName = styled.span`
+  color: #ff9000;
 `;
