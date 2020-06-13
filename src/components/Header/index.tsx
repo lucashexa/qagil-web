9436d9c678f645c32eab68d4dbd74012e5317409
Lucas Duarte <l.duarte.mk@gmail.com>
2020-06-13 15:50:12 -0300

    add image for user

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logoImg from '../../assets/logo.png';
 import { AiOutlinePoweroff } from 'react-icons/all';
-import { useUserBackend } from '../../hooks/userBackend';
 import { useAuth } from '../../hooks/auth';
+import { useUserBackend } from '../../hooks/userBackend';
+import { apiQimage, apiQuser } from '../../services/api';
 
 import {
   Container,
@@ -12,10 +13,72 @@ import {
   UserName,
 } from './styles';
 
+interface IuserBackEnd {
+  user_id: number;
+}
+
+interface fileResponse {
+  file_name: string;
+  url: string;
+}
+
 const Header: React.FC = () => {
-  const { userBackEnd }: any = useUserBackend();
   const { singOut, user }: any = useAuth();
-  console.log(user);
+  const { userBackEnd } = useUserBackend();
+  const [userId, setUserId] = useState<number>();
+  const [fileResponse, setFileResponse] = useState<fileResponse>(
+    {} as fileResponse,
+  );
+
+  if (userBackEnd) {
+    const { user_id } = userBackEnd as IuserBackEnd;
+    if (!userId) {
+      setUserId(user_id);
+    }
+  }
+
+  const backgroundCSS = {
+    backgroundImage: `url("${
+      fileResponse.url
+        ? fileResponse.url
+        : 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
+    } "`,
+    backgroundPosition: 'center',
+    height: '60px',
+    width: '60px',
+    borderRadius: '50%',
+    backgroundSize: 'cover',
+    cursor: 'pointer',
+    backgroundRepeat: 'no-repeat',
+  };
+
+  const inputCSS = {
+    opacity: 0,
+    height: '60px',
+    width: '60px',
+    borderRadius: '50%',
+    backgroundSize: 'cover',
+    cursor: 'pointer',
+    margin: 'auto',
+    marginTop: '10px',
+  };
+
+  const handleFile = async (e: any) => {
+    const formData = new FormData();
+
+    formData.append('file', e.target.files[0]);
+
+    const config = {
+      headers: {
+        apikey: process.env.REACT_APP_API_KEY,
+        user_id: userId,
+        type: 'qagile-art-profile',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    };
+    const response = await apiQimage.post('/v1', formData, config);
+    await setFileResponse(response.data);
+  };
 
   return (
     <Container>
@@ -23,15 +86,17 @@ const Header: React.FC = () => {
         <Logo src={logoImg} alt="QAgil" />
 
         <ProfileInfos>
-          <img
-            src={
-              'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
-            }
-            alt="profile Picture"
-          />
+          <div style={backgroundCSS}>
+            <input
+              style={inputCSS}
+              name="file"
+              type="file"
+              onChange={(e) => handleFile(e)}
+            />
+          </div>
           <div>
             <span>Bem Vindo</span>
-            {userBackEnd && <UserName>{user.name}</UserName>}
+            <UserName>{user.name}</UserName>
           </div>
         </ProfileInfos>
       </SubContainer>
